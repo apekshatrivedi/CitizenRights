0a31aa4a5411fe74ce3ce51805a81afca5a56406
apekshatrivedi <apekshatrivedi@yahoo.co.in>
2017-07-14 10:34:10 +0530

    design changes

--- app/src/main/java/com/grid/appy/citizenrights/activity/NewissueActivity.java
+++ app/src/main/java/com/grid/appy/citizenrights/activity/NewissueActivity.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.grid.appy.citizenrights.R;
 import com.grid.appy.citizenrights.activity.HomeActivity;
 import com.grid.appy.citizenrights.fragment.DatePickerFragment;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class NewissueActivity extends AppCompatActivity  {
+
+public class NewissueActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
 
@@ -33,11 +40,58 @@ public class NewissueActivity extends AppCompatActivity  {
                 startActivity(issues);
             }
         });
+
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Education");
+        categories.add("Work");
+        categories.add("Government");
+        categories.add("Bank");
+        categories.add("Retail");
+        categories.add("NGO");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "datePicker");
     }
+
+
+
+
+
+
+
 
 }
