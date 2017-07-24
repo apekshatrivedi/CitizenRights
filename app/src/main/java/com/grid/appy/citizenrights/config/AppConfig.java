74de80f35a68bc5220e910b3e26c33ad6708ee13
apekshatrivedi <apekshatrivedi@yahoo.co.in>
2017-07-24 12:38:53 +0530

    login

---
+++ app/src/main/java/com/grid/appy/citizenrights/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.grid.appy.citizenrights.config;
+
+/**
+ * Created by Appy on 24-Jul-17.
+ */
+
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/grid/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/grid/register.php";
+}
+
+
