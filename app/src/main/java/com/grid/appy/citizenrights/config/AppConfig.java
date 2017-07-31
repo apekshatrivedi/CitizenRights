794fffd32ae3ca3433670de04783a500cb8d62bf
RahulMKashyap <30317166+RahulMKashyap@users.noreply.github.com>
2017-07-31 12:55:04 +0530

    deptregis

--- app/src/main/java/com/grid/appy/citizenrights/config/AppConfig.java
+++ app/src/main/java/com/grid/appy/citizenrights/config/AppConfig.java
@@ -1,32 +1,30 @@
 package com.grid.appy.citizenrights.config;
-
-/**
- * Created by Appy on 24-Jul-17.
+/* Created by Appy on 24-Jul-17.
  */
 
 public class AppConfig {
 
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.101/grid/login.php";
+    public static String URL_LOGIN = "http://192.168.1.103/grid/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.101/grid/register.php";
+    public static String URL_REGISTER = "http://192.168.1.103/grid/register.php";
 
-    public static String URL_DREGISTER = "http://192.168.1.101/grid/register1.php";
+    public static String URL_DREGISTER = "http://192.168.1.103/grid/register1.php";
 
-    public static String SERVER_URL = "http://192.168.1.101/grid/upload.php";
+    public static String SERVER_URL = "http://192.168.1.103/grid/upload.php";
 
-    public static String UPLOADMYSQL_URL = "http://192.168.1.101/grid/uploadmysql.php";
+    public static String UPLOADMYSQL_URL = "http://192.168.1.103/grid/uploadmysql.php";
 
-    public static String GET_JSON_DATA_HTTP_URL = "http://192.168.1.101/Grid/ImageJsonData.php";
+    public static String GET_JSON_DATA_HTTP_URL = "http://192.168.1.103/Grid/ImageJsonData.php";
 
-    public static String GET_JSON_DATA_HTTP_URL1 = "http://192.168.1.101/Grid/homejson.php";
-
-    public static String GET_JSON_DATA_HTTP_URL2 = "http://192.168.1.101/Grid/issuehistory.php";
+    public static String GET_JSON_DATA_HTTP_URL1 = "http://192.168.1.103/Grid/homejson.php";
             //Tags used in the JSON String
 
 
-    public static String GET_ISSUE_DATA = "http://192.168.1.101/Grid/issuedetails.php";
+    public static String GET_ISSUE_DATA = "http://192.168.1.103/Grid/issuedetails.php";
+
+    public static String HTTP_JSON_URL="http://192.168.1.103/grid/ImageJsonData.php";
 
 
 }
