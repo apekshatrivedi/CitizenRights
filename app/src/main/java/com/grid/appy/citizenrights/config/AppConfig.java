e5c472d45723676c0a305a6e4aab919b4f7d2d15
apekshatrivedi <apekshatrivedi@yahoo.co.in>
2017-08-01 10:32:51 +0530

    deptlogin

--- app/src/main/java/com/grid/appy/citizenrights/config/AppConfig.java
+++ app/src/main/java/com/grid/appy/citizenrights/config/AppConfig.java
@@ -7,28 +7,33 @@ package com.grid.appy.citizenrights.config;
 public class AppConfig {
 
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.6/grid/login.php";
+    public static String URL_LOGIN = "http://192.168.1.100/grid/login.php";
 
-    public static String URL_LOGIN1 = "http://192.168.1.6/grid/loginreg.php";
+
+
+
+
+
+    public static String URL_LOGIN1 = "http://192.168.1.100/grid/loginreg.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.6/grid/register.php";
+    public static String URL_REGISTER = "http://192.168.1.100/grid/register.php";
 
-    public static String URL_DREGISTER = "http://192.168.1.6/grid/register1.php";
+    public static String URL_DREGISTER = "http://192.168.1.100/grid/register1.php";
 
-    public static String SERVER_URL = "http://192.168.1.6/grid/upload.php";
+    public static String SERVER_URL = "http://192.168.1.100/grid/upload.php";
 
-    public static String UPLOADMYSQL_URL = "http://192.168.1.6/grid/uploadmysql.php";
+    public static String UPLOADMYSQL_URL = "http://192.168.1.100/grid/uploadmysql.php";
 
-    public static String GET_JSON_DATA_HTTP_URL = "http://192.168.1.6/Grid/ImageJsonData.php";
+    public static String GET_JSON_DATA_HTTP_URL = "http://192.168.1.100/Grid/ImageJsonData.php";
 
-    public static String GET_JSON_DATA_HTTP_URL1 = "http://192.168.1.6/Grid/homejson.php";
+    public static String GET_JSON_DATA_HTTP_URL1 = "http://192.168.1.100/Grid/homejson.php";
 
-    public static String GET_JSON_DATA_HTTP_URL2 = "http://192.168.1.6/Grid/issuehistory.php?useremail=";
+    public static String GET_JSON_DATA_HTTP_URL2 = "http://192.168.1.100/Grid/issuehistory.php?useremail=";
             //Tags used in the JSON String
 
 
-    public static String GET_ISSUE_DATA = "http://192.168.1.6/Grid/issuedetails.php";
+    public static String GET_ISSUE_DATA = "http://192.168.1.100/Grid/issuedetails.php";
 
 
 }
